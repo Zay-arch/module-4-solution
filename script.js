7ed6933583f00e26a59e84454f6febcd412fb4c7
Zay-arch <76548558+Zay-arch@users.noreply.github.com>
2021-01-26 15:00:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ byeSpeaker();
 
    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-   for (var name in names) {
+   for (var i = 0; i < names.length; i++) {
 
       var firstLetter = names[name].charAt(0).toLowerCase();
 
